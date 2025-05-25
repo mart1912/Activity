2f06ed85ad112b533dce2602922b02c8b5e44325
mart1912 <hayassalvador1912@gmail.com>
2025-05-25 23:59:34 +0400

    perf: the code is optimized and passes the time limit

--- COTP/preimushchestvo.cpp
+++ COTP/preimushchestvo.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector <int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    vector <int> pr(n + 1);
+    for (int i = 0; i < n; i++)
+        pr[i + 1] = max(pr[i], a[i]);
+    vector <int> su(n + 1);
+    for (int i = n - 1; i >= 0; i--)
+        su[i] = max(su[i + 1], a[i]);
+    for (int i = 0; i < n; i++){
+        int mx = max(pr[i], su[i + 1]);
+        cout << a[i] - mx << " ";
+    }
+    cout << endl;
+}
 int main(){
     int t;
     cin >> t;
-    for (int q = 0; q < t; q++) {
-        int n;
-        cin >> n;
-        vector <int> a(n);
-        for (int j = 0; j < n; j++) {
-            int s;
-            cin >> s;
-            a[j] = s;
-        }
-        vector <int> max(n);
-        vector <int> ans(n);
-        for (int p = 0; p < n; p++) {
-            for (int i = 0; i < n; i++) {
-                if (p != i) {
-                    if (max[p] < a[i]) max[p] = a[i];
-                }
-            }
-            ans[p] = a[p] - max[p];
-        }
-        for (int b = 0; b < n; b++) {
-            cout << ans[b] << " ";
-        }
-        cout << '\n';
-    }
+    while (t--)
+        solve();
     return 0;
 }
