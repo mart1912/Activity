7564519e5e3b364fd449edbc0e432c2657795e7b
mart1912 <hayassalvador1912@gmail.com>
2025-05-26 00:12:28 +0400

    feat: add problem about finding out whether a given undirected graph is a tree

---
+++ COTP/a_bilo_li_derevo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<vector<int>> g;
+vector<char> used;
+vector<int> p;
+
+void dfs(int v) {
+	used[v] = true;
+	for (int u : g[v]) {
+		if (!used[u]) {
+
+			dfs(u);
+		}
+	}
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+	g.assign(n, {});
+	for (int i = 0; i < m; i++) {
+		int v, u;
+		cin >> v >> u;
+		--v; --u;
+
+		g[v].push_back(u);
+		g[u].push_back(v);
+	}
+
+	used.assign(n, false);
+	dfs(0);
+	
+	if (m == n - 1 && all_of(used.begin(), used.end(), [](char i) {return i; })) cout << "YES\n";
+	else cout << "NO\n";
+	system("pause");
+}
