e4ec680cdc3ca47445a4cb728e9a71ac9f390d92
mart1912 <hayassalvador1912@gmail.com>
2025-05-26 00:24:26 +0400

    fix: add required conditions

--- COTP/pasha_i_palka.cpp
+++ COTP/pasha_i_palka.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	if (n % 4 == 0) cout << n / 4 - 1;
-	else cout << n / 4;
+	if (n % 2 == 0) {
+		if (n % 4 == 0) cout << n / 4 - 1;
+		else cout << n / 4;
+	}
+	else cout << 0;
 	return 0;
 }
